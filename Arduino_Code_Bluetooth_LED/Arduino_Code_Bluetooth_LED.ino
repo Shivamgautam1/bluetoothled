7e74eb1c8f1ea3a5b6e1e6184366df1930a5fe71
Shivam Gautam <shivamgautam1@gmail.com>
2018-03-17 13:27:57 +0530

    Arduino Code

---
+++ Arduino_Code_Bluetooth_LED/Arduino_Code_Bluetooth_LED.ino
@@ -0,0 +1,29 @@
+/* Example Bluetooth LED code
+ *  Code Written by Shivam Gautam (www.amritsar.tech)
+ *  www.github.io/shivamgautam1
+ *  Install the app from the github repo (Source code included)
+ *  www.github.io/shivamgautam1/bluetoothled
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+int ledpin=13;
+int BluetoothData;
+
+void setup() {
+  mySerial.begin(9600);
+  pinMode(ledpin,OUTPUT);
+}
+
+void loop() {
+   if (mySerial.available()){
+      BluetoothData=mySerial.read();
+      if(BluetoothData=='1'){ 
+         digitalWrite(ledpin,1);
+      }
+      if (BluetoothData=='0'){
+      digitalWrite(ledpin,0);
+      }
+    }
+  delay(100);
+}
